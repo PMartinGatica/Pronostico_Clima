bae3fd2ae9be424af0801c8fb404cc967dd36c94
Pablo Gatica <ga77ica@hotmail.com>
2022-11-09 23:27:17 -0300

    Init iconos Clima v1.2

--- script/app.js
+++ script/app.js
@@ -120,34 +120,19 @@ function climaExtendido (datos){
             //velocidad del viento maxima del dia
             let velocidadVientoMaxima = (Math.max(...windspeed_10m.slice(index*24,(index+1)*24)));
             console.log(velocidadVientoMaxima);
+            let icono = iconoPrecipitaciones(precipitacionTotal);
+            console.log(icono);
         }
     }
 
-    function iconoPrecipitaciones(datos){
-        const { hourly:{temperature_2m,precipitation} } = datos;
-        switch (temperature_2m,precipitation) {
-            case (temperature_2m < 0 && precipitation >60):
-                console.log('Lluvia Torrenciales');
-                break;
-            case (temperature_2m < 0 && precipitation >30):
-                console.log('Lluvia muy fuerte');
-                break;
-                case (temperature_2m < 0  && precipitation >15):
-                    console.log('Lluvia Fuertes');
-                    break;
-                case (temperature_2m < 0 && precipitation >2):
-                    console.log('Lluvia');
-                    break;
-                case (temperature_2m < 10 && precipitation < 2):
-                    console.log('Lluvia Debil');
-                    break;
-                case (temperature_2m > 11 && precipitation <= 0):
-                    console.log('Nubes');
-                    break;
-                default:
-                console.log('Sol');
-                break;
-        }
+    function iconoPrecipitaciones(precipitations){
+        if (precipitations === 0) 
+        {return `src='./images/icons/animated/clear-day.svg' alt='soleado'`;}
+
+        if (precipitations <= 2) {return `src='./images/icons/animated/rainy-1-day.svg' alt='Lluvias débiles'`;}
+        if (precipitations <= 30) {return `src='./images/icons/animated/rainy-2-day.svg' alt='Lluvia'`;};
+        if (precipitations <= 60) {return `src='./images/icons/animated/rainy-3-day.svg' alt='Lluvias muy fuertes'`}
+        else {return `src='./images/icons/animated/rainy-3-day.svg' alt='Lluvias muy fuertes'`;}
     }
 
 
