f68d5c07c86e05028c9e157459da354207f69f7b
PMartinGatica <pablomartin.gatica@gmail.com>
2022-11-11 09:08:33 -0300

    HTML V1.4

--- script/app.js
+++ script/app.js
@@ -37,45 +37,64 @@ function climaActual(datos){
         let velocidadVientoMaxima = (Math.max(...windspeed_10m.slice(0,24)));
         // console.log(velocidadVientoMaxima);    
         let icono = iconoPrecipitaciones(precipitacionTotal);
+
+        temperaturaActual.innerHTML += TemperaturaActual(icono ,temperaturaMaxima,velocidadVientoMaxima);
         
-        const iconoClima = document.createElement('div');
-        iconoClima.innerHTML = `${icono}`;
+        // const row = document.createElement('div')
+        // row.classList.add("row mt-3")
+        // const divCol = document.createElement('div')
+        // divCol.classList.add('col');
+        // divCol.classList.add('d-flex');
+        // divCol.classList.add('justify-content-center');
+        // divCol.classList.add('align-items-center');
+        // const divCard = document.createElement('div');
+        // divCard.classList.add('card')
+        // const iconoClima = document.createElement('div');
+        // iconoClima.innerHTML = `${icono}`;
+        // const divCardBody = document.createElement('div');
+        // divCardBody.classList.add('card-body')
 
-        const fechaActual = document.createElement('p');
-        fechaActual.innerHTML = `Fecha de Hoy: ${fechaDate}`;
-        fechaActual.classList.add('text-xl')
 
-        const tempMaxima = document.createElement('p');
-        tempMaxima.innerHTML = `Temperatura Max: ${temperaturaMaxima}&#8451;`;
-        tempMaxima.classList.add('text-xl')
+        // // const fechaActual = document.createElement('p');
+        // // fechaActual.innerHTML = `Fecha de Hoy: ${fechaDate}`;
+        // // fechaActual.classList.add('text-xl')
+
+        // const tempMaxima = document.createElement('h5');
+        // tempMaxima.innerHTML = `${temperaturaMaxima}&#8451;`;
+        // tempMaxima.classList.add('card-title')
 
 
-        const tempMinima = document.createElement('p');
-        tempMinima.innerHTML = `Temperatura Min: ${temperaturaMinima} &#8451;`;
-        tempMinima.classList.add('text-xl')
+        // // const tempMinima = document.createElement('p');
+        // // tempMinima.innerHTML = `Temperatura Min: ${temperaturaMinima} &#8451;`;
+        // // tempMinima.classList.add('text-xl')
 
-        const sensacionTerm = document.createElement('p');
-        sensacionTerm.innerHTML = `Sensacion Termica: ${sensacionTermicaMaxima} &#8451;`;
-        sensacionTerm.classList.add('text-xl')
+        // // const sensacionTerm = document.createElement('p');
+        // // sensacionTerm.innerHTML = `Sensacion Termica: ${sensacionTermicaMaxima} &#8451;`;
+        // // sensacionTerm.classList.add('text-xl')
 
-        const precipitacionTot = document.createElement('p');
-        precipitacionTot.innerHTML = `Lluvia : ${precipitacionTotal} mm`;
-        precipitacionTot.classList.add('text-xl')
+        // // const precipitacionTot = document.createElement('p');
+        // // precipitacionTot.innerHTML = ` : ${precipitacionTotal} mm`;
+        // // precipitacionTot.classList.add('text-xl')
         
-        const vientoMax = document.createElement('p');
-        vientoMax.innerHTML = `Viento: ${velocidadVientoMaxima} km/h;`;
-        vientoMax.classList.add('text-xl')
+        // const vientoMax = document.createElement('h6');
+        // vientoMax.innerHTML = `Viento: ${velocidadVientoMaxima} km/h;`;
+        // vientoMax.classList.add('card-subtitle')
+        // vientoMax.classList.add('text-muted')
+        // vientoMax.classList.add('mb-2')
 
-        const resultadoDiv = document.createElement('div');
-        resultadoDiv.classList.add('text-center', 'text-white')
-        resultadoDiv.appendChild(iconoClima);
-        resultadoDiv.appendChild(fechaActual);
-        resultadoDiv.appendChild(tempMaxima);
-        resultadoDiv.appendChild(tempMinima);
-        resultadoDiv.appendChild(sensacionTerm);
-        resultadoDiv.appendChild(precipitacionTot);
-        resultadoDiv.appendChild(vientoMax);
-        resultado.appendChild(resultadoDiv)
+        // const resultadoDiv = document.createElement('div')
+        // resultadoDiv.classList.add("row")
+        // resultadoDiv.classList.add("mt-3")
+        // resultadoDiv.appendChild(divCol);
+        // resultadoDiv.appendChild(divCard);
+        // resultadoDiv.appendChild(iconoClima);
+        // resultadoDiv.appendChild(divCardBody);
+        // resultadoDiv.appendChild(tempMaxima);
+        // // resultadoDiv.appendChild(tempMinima);
+        // // resultadoDiv.appendChild(sensacionTerm);
+        // // resultadoDiv.appendChild(precipitacionTot);
+        // resultadoDiv.appendChild(vientoMax);
+        // resultado.appendChild(resultadoDiv)
 
 }
 
@@ -99,8 +118,6 @@ function formatoFechaSemanal(fecha){
     }
     return dateSem;
 }
-
-
 function climaExtendido (datos){
     const { hourly:{time,temperature_2m, apparent_temperature, precipitation, windspeed_10m} } = datos;
 
@@ -171,10 +188,35 @@ function climaExtendido (datos){
         if (precipitations === 0) 
         {return `<img src="./img/animated/clear-day.svg" alt="Soleado">`;}
 
-        if (precipitations <= 2) {return `<img src='./img/animated/rainy-1-day.svg' alt='Lluvias débiles'>`;}
-        if (precipitations <= 30) {return `<img src='./img/animated/rainy-2-day.svg' alt='Lluvia'>`;};
-        if (precipitations <= 60) {return `<img src='./img/animated/rainy-3-day.svg' alt='Lluvias muy fuertes'>`}
-        else {return `<img src='./img/animated/scattered-thunderstorms-day.svg' alt='Lluvias muy fuertes'>`;}
+        if (precipitations <= 2) {return `<img class="card-img-top" src='./img/animated/rainy-1-day.svg' alt='Lluvias débiles'>`;}
+        if (precipitations <= 30) {return `<img class="card-img-top" src='./img/animated/rainy-2-day.svg' alt='Lluvia'>`;};
+        if (precipitations <= 60) {return `<img class="card-img-top" src='./img/animated/rainy-3-day.svg' alt='Lluvias muy fuertes'>`}
+        else {return `<img class="card-img-top" src='./img/animated/scattered-thunderstorms-day.svg' alt='Lluvias muy fuertes'>`;}
     }
+
+    const TemperaturaActual = (
+        icono,
+        temperaturaMaxima,
+        velocidadVientoMaxima
+      ) => {
+        let result = "" 
+        return (result += `
+        <div class="container temperaturaActual">
+        <div class="row mt-3">
+            <div class="col d-flex justify-content-center align-items-center">
+                <div class="card tarjetaActual">
+                    ${icono}
+                    <div class="card-body"><h5 class="card-title mb-3">Temperatura</h5>
+                        <h5 class="card-title mb-3 fw-bold text-uppercase text-align-center">${
+                            temperaturaMaxima
+                           } °C </h5>
+                        <h6 class="card-subtitle text-muted mb-2">Viento : ${velocidadVientoMaxima} km/h </h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+              `);
+      };
 
 
